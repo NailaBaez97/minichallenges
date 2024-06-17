37bdde0d24f6971ebf8ea3d5ae91110308ee6610
nailabaez97 <naiibaezr65@gmail.com>
2024-06-17 19:29:49 -0400

    Mini challenge dia 5 : Crear Diccionario

--- dia5.py
+++ dia5.py
@@ -1,4 +1,8 @@
+#Crear un Diccionario: Escribe un programa que crea un diccionario a partir de dos listas dadas:
+# una de claves y otra de valores.
 clave = input("Ingresar la clave: ")
 valor = input("Ingresar el valor: ")
+clave= clave.split(",")
+valor = valor.split(",")
 mi_diccionario = dict(zip(clave,valor))
 print(mi_diccionario)
