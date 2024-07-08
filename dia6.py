709ed177e35fbc9e50a9f6d9be19cb864dfd1b5a
nailabaez97 <naiibaezr65@gmail.com>
2024-07-08 14:53:42 -0400

    Eliminar archivo

--- dia6.py
+++
@@ -1,4 +0,0 @@
-#Conversión de temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
-celsius = int(input("Ingrese una temperatura en grado Celsius: "))
-fahrenheit = celsius * 9/5 + 32
-print("La temperatura en grado Fahreinheit es: ",fahrenheit)
