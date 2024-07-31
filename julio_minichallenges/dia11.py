418fd669040bda83b12f07b6208d134aff0af735
nailabaez97 <naiibaezr65@gmail.com>
2024-07-31 16:45:52 -0400

    Dia 11: Clase de Punto 2D

---
+++ julio_minichallenges/dia11.py
@@ -0,0 +1,13 @@
+#Clase de Punto 2D: Crea una clase Punto2D con atributos x& y, y un método para imprimir sus coordenadas.
+class Punto2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def imprimir_coordenadas(self):
+        print(f"Coordenadas del punto: ({self.x}, {self.y})")
+
+if __name__ == '__main__':
+    punto = Punto2D(3, 4)
+    punto.imprimir_coordenadas()  
+
