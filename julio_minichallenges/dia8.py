7ddafaf10abd3e4e05e7ecec8008d7b85ac28171
nailabaez97 <naiibaezr65@gmail.com>
2024-07-31 16:42:53 -0400

    Dia 8: Pilas y colas

---
+++ julio_minichallenges/dia8.py
@@ -0,0 +1,16 @@
+#Pilas y colas: Implementa las operaciones básicas de una pila y/o una cola para 5 elementos.
+
+pila =[]
+    #agregando elementos por el final
+pila.append(1)
+pila.append(2)
+pila.append(3)
+pila.append(4)
+pila.append(5)
+print(pila)
+
+    #quitando elementos por el final
+
+pila.pop()
+
+print(pila)
