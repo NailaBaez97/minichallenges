2949c6b4d5f0dda1568eb8ac0621fccd0cde6737
nailabaez97 <naiibaezr65@gmail.com>
2024-07-31 16:58:16 -0400

    Dia 20: Actualizar registros

---
+++ julio_minichallenges/dia20.sql
@@ -0,0 +1,4 @@
+#Actualizar registros: Escribe una consulta SQL para actualizar el nombre de un usuario específico en la tabla Usuarios.
+UPDATE usuarios
+SET nombre_usuarios = 'Jazmin'
+where id_usuarios = 1;
